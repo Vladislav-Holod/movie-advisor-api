e77607c12c510e15ed2c861dea44ad43a704638a
holod <vladisjack22@gmail.com>
2026-06-22 13:10:08 +0300

    requirements.txt

--- app/models/association_tables.py
+++ app/models/association_tables.py
@@ -7,6 +7,7 @@ user_favorite_books = Table(
     Column("user_id", ForeignKey("users.id"), primary_key=True),
     Column("book_id", ForeignKey("books.id"), primary_key=True),
 )
+
 user_profile_liked_books = Table(
     "user_profile_liked_books",
     Base.metadata,
