920076c258f3c2f966c5e4831dbaa6d484b06c51
holod <vladisjack22@gmail.com>
2026-06-25 14:03:22 +0300

    movie 25.06.2026 gemeni

--- app/routes/movie_recomend.py
+++ app/routes/movie_recomend.py
@@ -1,18 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.schemas import Movie
 from app.dependencies import ai_client
 from app.dependencies import api_kinopoisk
 from typing import List
 import asyncio
-
+from app.db_depends import get_async_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models import MovieModel
+from sqlalchemy import select
 router = APIRouter(
     prefix='/movie/recommend',
     tags=['AI']
 )
 
 
-@router.post('/', response_model=list)
-async def recommend_movie(prompt: str):
+@router.post('/', response_model=list[Movie])
+async def recommend_movie(prompt: str, db: AsyncSession = Depends(get_async_db)):
     filter_sets = await ai_client.ai_response_search_filters(prompt)
     tasks = [api_kinopoisk.search_by_filters(f, limit=15) for f in filter_sets]
     results = await asyncio.gather(*tasks, return_exceptions=True)
@@ -27,4 +30,20 @@ async def recommend_movie(prompt: str):
                 seen.add(movie.id_pois)
                 all_movies.append(movie)
     result_movie_for_client = await ai_client.ai_search_best_movie(all_movies, prompt)
+
+    movie_ids = [movie.id_pois for movie in result_movie_for_client]
+
+    existing_ids = set(
+        (await db.execute(
+                select(MovieModel.id_pois).where(
+                    MovieModel.id_pois.in_(movie_ids)))).scalars().all())
+    new_movies = [
+        MovieModel(**movie.model_dump())
+        for movie in result_movie_for_client
+        if movie.id_pois not in existing_ids
+    ]
+    if new_movies:
+        db.add_all(new_movies)
+        await db.commit()
+
     return result_movie_for_client
